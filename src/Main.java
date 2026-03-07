e3ce18893149114eb005ac11dbd004fe846c3231
Manikandan M P <manikandanmperikamana@gmail.com>
2026-03-07 16:56:12 +0530

    Main class:create vehicle spots and 3 vehicles entry.Can print the ticket for the vehicles entered and parked.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
 import model.*;
+import service.ParkingLot;
+import strategy.*;
+import java.util.ArrayList;
+import java.util.List;
 public class Main {
     public static void main(String[] args) {
-        Vehicle car=new Car("KA01AB1234");
-        Vehicle bike=new Bike("KA01AB1233");
-        System.out.println(car.calculateParkingFee(3));
-        System.out.println(bike.calculateParkingFee(3));
+        List<ParkingSpot> spots=new ArrayList<>();
+        spots.add(new CarSpot("C1"));
+        spots.add(new CarSpot("C2"));
+        spots.add(new BikeSpot("B1"));
+        spots.add(new TruckSpot("T1"));
+        
+        ParkingLot parkingLot=new ParkingLot(spots);
+        Vehicle car=new Car("KA53AB1234");
+        Vehicle bike=new Bike("KA53AB1222");
+        Vehicle truck=new Truck("KA53AB1244");
+
+        Ticket carTicket=parkingLot.parkVehicle(car);
+        Ticket bikeTicket=parkingLot.parkVehicle(bike);
+        System.out.println(carTicket);
+        System.out.println(bikeTicket);
+
+        PaymentStrategy payment=new UpiPayment();
+        PaymentReceipt receipt=parkingLot.exitVehicle("KA53AB1234", payment);
+        System.out.println(receipt);
     }
 }
